d38c51acfa81bd1fd50f81fbea3a33e553ed7fe9
FCalic07 <97953488+FCalic07@users.noreply.github.com>
2022-10-15 19:13:14 +0200

    Update PrviZadatak.c

--- PrviZadatak.c
+++ PrviZadatak.c
@@ -15,8 +15,8 @@ typedef struct _students {
 	int points;
 } Student; //Onda ovdje veliko slovo jer je ovo zapravo ime strukture, pa ono kao ime poc.vel.slovom
 
-int CountStudents();
-Student* Allocation(int num, Student* s);
+int CountStudents(char* fname);
+Student* Allocation(int num, Student* s, char* fname);
 int Printing(int num, Student* s); // NE SMIJU SE KORISTITI VOID FUNKCIJE NA OVOM PREDMETU, MORA VRACATI NESTO BAR 0 ILI NULL
 double RelativePoints(int pts);
 
@@ -24,11 +24,19 @@ double RelativePoints(int pts);
 int main() {
 	Student* studenti = NULL;
 	int NumOfStudents = 0;
+	char name_buffer[MAX_NAME] = { 0 };
+	char* file_name = 0;
+	file_name = &name_buffer;
+	/*Gledajuci tvoj kod od prosle godine cini mi se da i treba se ime datoteke prenosit
+	pa nek ono pita korisnika kako se zove on ukuca tipa studenti.txt i odradi se sve*/
+	printf("\nEnter file name: ");
+	gets_s(name_buffer, MAX_NAME); /*isto ko i scanf jel jer moram svakako na pokazivac prebacivat, kako zelis*/
+
 	/* pozvati funkciju koja racuna broj studenata u datoteci*/
-	NumOfStudents = CountStudents();
+	NumOfStudents = CountStudents(file_name);
 
 	/* pozvati funkciju koja alocira i reda ih u strukturu*/
-	studenti = Allocation(NumOfStudents, studenti);
+	studenti = Allocation(NumOfStudents, studenti, file_name);
 
 	/*Pozvati funkciju da printa ovo*/
 	Printing(NumOfStudents, studenti);
@@ -36,11 +44,11 @@ int main() {
 	return EXIT_SUCCESS; // u mainu je dobro koristiti EXIT_SUCCESS jer on obavlja jos neke provjere pri izlazu iz maina
 }
 
-int CountStudents() {
+int CountStudents(char* fname) {
 	int count = 0;
 	char buffer[MAX_SIZE] = { 0 };
 	FILE* fp = NULL;
-	fp = fopen("studenti.txt", "r");
+	fp = fopen(fname, "r");
 
 	if (fp == NULL) {
 		printf("File can not open!\n");
@@ -59,9 +67,9 @@ int CountStudents() {
 	return count;
 }
 
-Student* Allocation(int num, Student* s) {
+Student* Allocation(int num, Student* s, char* fname) {
 	FILE* fp = NULL;
-	fp = fopen("studenti.txt", "r");
+	fp = fopen(fname, "r");
 	char buffer[MAX_SIZE] = { 0 };
 	s = (Student*)malloc(num * sizeof(Student));
 	int i = 0; //ovo je c,a ne c++ pa bi sve deklaracije i inicijalizacije trebale biti na vrhu funkcije, no u vecini slucajeva moze i onako kako si ti
@@ -73,7 +81,7 @@ Student* Allocation(int num, Student* s) {
 
 
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	if (!s) //moras provjeriti je li uspjesna alokacija
+	if (!s) //moras provjeriti je li uspjesna alokacija   (pametno)
 	{
 		printf("Greska alokacije\n");
 		return NULL;
@@ -82,12 +90,8 @@ Student* Allocation(int num, Student* s) {
 
 
 	while (!feof(fp)) {
-		fgets(buffer, MAX_SIZE, fp);
-		if (strcmp(buffer, "\n") != 0) {
-			sscanf(buffer, "%s %s %d", s[i].name, s[i].surname, &s[i].points); /*ovo si mi ti spominjao i vid fakat radi*/ 
-																			   //dobro to ovdje jos ne treba jer je ucitavanje u strukturu a ne u vezanu listu, ono....to cemo raditi iduci sat pa je sad logicinije fscanf, jer je manje tako posla
-			i++;
-		}
+		 fscanf(fp,"%s %s %d", s[i].name, s[i].surname, &s[i].points);
+		 i++;    /*da da, lakse ovo, nez sto sam mislio da fscanf ce uzet nesto iz prazne linije pf*/
 	}
 
 	fclose(fp);
