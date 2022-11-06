b8b91deca8ed2733ecc250609c846aa37ca9df19
FCalic07 <97953488+FCalic07@users.noreply.github.com>
2022-11-06 18:25:36 +0100

    Update CetvrtiTestnaFaza.c

--- CetvrtiTestnaFaza.c
+++ CetvrtiTestnaFaza.c
@@ -1,5 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable : 4996)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,11 +28,15 @@ typedef struct _polynominal {
 int EnterFilename(char* filename);
 int ExtractFromFile(PPosition pHead, char* filename);
 int CreatePoly(PPosition pHead, char* buffer);
+int PolyHeadElement(PPosition newPoly);
 PPosition FindLast(PPosition pHead);
 int AddAfterPoly(PPosition pos1, PPosition pos2);
 int CreateElement(PPosition Poly, int coef, int exp);
 EPosition FindLastElement(PPosition Poly);
 int AddAfterElement(EPosition pos1, EPosition pos2);
+
+//treba one sortove po velicini i da se zbrajaju i to za pojedinacne polinome
+//konacni zbroj i mnozenje
 
 int main() {
 	//prvo stvorit nulti polinom
@@ -44,7 +47,7 @@ int main() {
 	EnterFilename(filename); //ime datoteke
 
 	ExtractFromFile(pHead, filename); //ova funkcija sluzi da preko nje sve iz datoteke ide u listu
-	 
+
 	//da bi dokazao da mi radi napravio sam samo ovu funkciju
 	/*necu lagat, malo ponosan na sebe sto sam skonto ovo,
 	doduse sto jest jest ruzno je pa sad treba ic ka dekadenciji*/
@@ -64,7 +67,7 @@ int ExtractFromFile(PPosition pHead, char* filename) {
 	FILE* fp;
 	fp = fopen(filename, "r");
 	char buffer[MAX_LINE] = { 0 };
-	
+
 	if (!fp) {
 		printf("\nFile can not open!");
 		return FILE_OPENING_ERROR;
@@ -94,24 +97,37 @@ int CreatePoly(PPosition pHead, char* buffer) {
 		return ALLOCATION_ERROR;
 	}
 	//sad treba napravit njegov nulti element
-	EPosition newElement = NULL;
-	newElement = (EPosition)malloc(sizeof(Element));
-	newElement->coef = 0;
-	newElement->exp = 0;
-	newElement->elNext = NULL;
-	newPoly->elNext = newElement;
+	PolyHeadElement(newPoly);
 
 	Last = FindLast(pHead); //nalazi zadnji, klasika
 	AddAfterPoly(Last, newPoly);//ide zadnje uvijek jer takvim redom ih kupi
 
 
-	//sad imamo poly -> treba citat iz buffera i stvarat element listu koja se veže za ovaj poly
+	//sad imamo poly -> treba citat iz buffera i stvarat element listu koja se veÅ¾e za ovaj poly
 	while (strlen(buffer) > 0) {
-		sscanf(buffer, "%d %d %n", &coef, &exp, &bytes);
-		CreateElement(newPoly, coef, exp); //kreiraj element
+		if ((sscanf(buffer, "%d %d %n", &coef, &exp, &bytes)) == 2) {
+			if(coef!=0)													//filter
+			CreateElement(newPoly, coef, exp); //kreiraj element
+		}
 		buffer += bytes;
 	}
 
+
+	return EXIT_SUCCESS;
+}
+
+int PolyHeadElement(PPosition newPoly) {
+	EPosition newElement = NULL;
+	newElement = (EPosition)malloc(sizeof(Element));
+	if (!newElement) {
+		printf("\nAllocation error.");
+		return ALLOCATION_ERROR;
+	}
+
+	newElement->coef = 0;
+	newElement->exp = 0;
+	newElement->elNext = NULL;
+	newPoly->elNext = newElement;
 
 	return EXIT_SUCCESS;
 }
@@ -153,13 +169,13 @@ int CreateElement(PPosition Poly, int coef, int exp) {
 
 EPosition FindLastElement(PPosition Poly) {
 	EPosition temp = Poly->elNext;
-	while (temp->elNext) { 
+	while (temp->elNext) {
 		temp = temp->elNext;
 	}
 	return temp;
 }
 
-int AddAfterElement(EPosition pos1,EPosition pos2) { /*jer ako nema onda pos1 u prvom slucaju ovo pozivanje je nemoguce zbog nullptr odnosno pos1 nema nikakve alocirane varijable*/
+int AddAfterElement(EPosition pos1, EPosition pos2) { /*jer ako nema onda pos1 u prvom slucaju ovo pozivanje je nemoguce zbog nullptr odnosno pos1 nema nikakve alocirane varijable*/
 	pos2->elNext = pos1->elNext;
 	pos1->elNext = pos2;
 	return EXIT_SUCCESS;
@@ -169,11 +185,11 @@ int PrintPolynominalList(PPosition pHead) {
 	PPosition temp1 = pHead->pNext;
 	EPosition temp2 = NULL;
 	int i = 1;
-	while (temp1){
+	while (temp1) {
 		printf("\n%d. -->	", i);
-		temp2 = temp1->elNext->elNext;   //caka je u tom što prilicno vjerujem da mora imat nulti element
+		temp2 = temp1->elNext->elNext;   //caka je u tom Å¡to prilicno vjerujem da mora imat nulti element
 		while (temp2) {
-			printf("coef: %d exp: %d	", temp2->coef, temp2->exp);
+			printf("%dx^%d	", temp2->coef, temp2->exp);
 			temp2 = temp2->elNext;
 		}
 		i++;
